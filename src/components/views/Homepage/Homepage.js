f48a6f0313812d5773cdbab8925eca1987b224a8
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-11-29 21:18:18 +0100

    Add changes to post add and render it in homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -12,50 +12,42 @@ import { getUser } from '../../../redux/userRedux.js';
 
 import styles from './Homepage.module.scss';
 
-class Component extends React {
+class Component extends React.Component {
   componentDidMount() {
     const {fetchPublishedPosts} = this.props;
     fetchPublishedPosts();
   }
   render() {
-    const { className, posts, user } = this.props;
+    const {className, posts, user} = this.props;
     return (
       <div className={clsx(className, styles.root)}>
         <CardGroup>
           {posts.map(post => (
             <Card className={styles.card_ad} key={post.id}>
-              <Card.Img className={styles.card_img} variant="top" src={post.img} />
+              {post.photo === null ? '' : <Card.Img className={styles.card_img} variant="top" src={post.img} />}
               <Card.Body>
-                <Card.Title><a href={`/post/${post.id}`}>{post.title}</a></Card.Title>
-                <Card.Text>
-              ${post.price}
-                </Card.Text>
-                <Card.Text>
-              Seller: {post.name}
-                </Card.Text>
-                <Card.Text>
-              City: {post.location}
-                </Card.Text>
-                <Card.Text>
-              phone: {post.phone}
-                </Card.Text>
+                <Card.Title><a href={`/post/${post._id}`}>{post.title}</a></Card.Title>
+                {post.price < 0 ? <Card.Text>${post.price}</Card.Text>: ''}
+                <Card.Text> By: {post.author} </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">Published {post.date}</small>
+                <small className="text-muted">Published {post.created}</small>
               </Card.Footer>
             </Card>
           ))}
         </CardGroup>
         {user.isLogged ?
-          <Button className={styles.form_button} href='post/add'size="lg" block variant="dark" type="submit">AddPost</Button> : ''}
+          <Button className={styles.form_button} href='post/add'size="lg" block variant="dark" type="submit">
+      Add Post
+          </Button>
+          : ''}
       </div>
-    );
-  }
+    );}
 }
 
 Component.propTypes = {
-  posts: PropTypes.array,
   className: PropTypes.string,
+  posts: PropTypes.array,
   user: PropTypes.object,
   fetchPublishedPosts: PropTypes.func,
 };
